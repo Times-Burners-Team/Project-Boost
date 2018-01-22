b4ada24c44bb3c5f7464d7791747cf0fc68d88fb
Dmitrii <timesburnersteam@gmail.com>
2018-01-21 21:01:50 -0800

    Added sounds

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -6,11 +6,12 @@ using UnityEngine;
 public class Rocket : MonoBehaviour {
 
 	Rigidbody rigidBody;
-	
+	AudioSource audioSource;
 	
 	// Use this for initialization
 	void Start () {
 		rigidBody = GetComponent<Rigidbody>();
+		audioSource = GetComponent<AudioSource>();
 	}
 	
 	// Update is called once per frame
@@ -23,8 +24,15 @@ public class Rocket : MonoBehaviour {
         if (Input.GetKey(KeyCode.Space)) // can thrust while rotating
 		{
 			rigidBody.AddRelativeForce(Vector3.up);
+			if (!audioSource.isPlaying)
+			{
+			audioSource.Play();
+			}
 		}
-
+		else
+		{
+			audioSource.Stop();
+		}
 		if (Input.GetKey(KeyCode.A)) 
 		{
 			transform.Rotate(Vector3.forward);
