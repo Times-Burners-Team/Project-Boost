2a40d74571b010d72e35eae6e328d95c704e5b1e
B3rl1 <kirill.skv.2000@mail.ru>
2018-01-29 11:05:04 +0300

    1

---
+++ Assets/MainMenu/Menu.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour {
+	public GameObject settings;
+	public GameObject start;
+
+	public void startGame() {
+		if (settings == !settings.activeSelf) {
+			start.SetActive (!start.activeSelf);
+		} else {
+			settings.SetActive (!settings.activeSelf);
+			start.SetActive (!start.activeSelf);
+		}
+	}
+
+	public void Settings(){
+		if (start == !start.activeSelf) {
+			settings.SetActive (!settings.activeSelf);
+		} else {
+			start.SetActive (!start.activeSelf);
+			settings.SetActive (!settings.activeSelf);
+		}
+
+	}
+
+	public void Exit(){
+
+		Application.Quit () ;
+	}
+}
+
